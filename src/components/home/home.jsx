45347533259d4c535e5b77c88a788e3df5a723fe
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-08 14:03:31 +0530

    Found modified or deleted file,  src/components/home/home.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import NavBar from "../common/nav-bar";
 import Hero from "./hero-section";
-import CounterSection from "./counter-section";
+import MuTopics from "./mu-topics";
 import OfficialLinks from "./official-links";
 import WhatsMulearn from "./whats-mulearn";
 import KeyPoints from "./key-points";
@@ -9,15 +8,17 @@ import ConnectCompeteLearn from "./connect-compete-learn";
 import LearnAndGrow from "./learn-grow";
 import FAQList from "../frequently-asked-questions/faq-list";
 import Footer from "../common/footer";
+import WhatsMulearnCampusChapters from "./whats-campus-chapters";
 
 function Home() {
   return (
     <div className="en-Oxanium">
       <NavBar />
       <Hero />
-      <CounterSection />
+      <MuTopics />
       <OfficialLinks />
       <WhatsMulearn />
+      <WhatsMulearnCampusChapters />
       <KeyPoints />
       <ConnectCompeteLearn />
       <LearnAndGrow />
