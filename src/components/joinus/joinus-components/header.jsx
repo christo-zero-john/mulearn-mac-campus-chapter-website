144c7e33d958170378f4d5ee35c41eddc4650e2f
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-08 14:03:32 +0530

    Found modified or deleted file,  src/components/joinus/joinus-components/header.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/components/joinus/joinus-components/header.jsx
+++ src/components/joinus/joinus-components/header.jsx
@@ -3,9 +3,9 @@ import imgWhatsapp from "../../../assets/img/social/whatsapp.svg";
 function Header() {
   function requestToJoin() {
     document.getElementById("requestDiv").innerHTML = `
-            <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=919495806285&text=Hello%2C%0AI%20want%20to%20join%20muLearn%20whatsapp%20group.%20"><img src="${imgWhatsapp}" alt="" class="whatsapp">  Christo John</a>
+            <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=919495806285&text=Hello%2C%20My%20name%20is%20%5BYour%20name%5D%20and%20I%20am%20%5Byour%20relationship%20with%20MAC%20Ramapuram%5D.%20I%20would%20like%20to%20join%20the%20muLearn%20whatsapp%20group%20of%20MAC%20Ramapuram.%20Please%20add%20me%20to%20the%20group.%20Thank%20you."><img src="${imgWhatsapp}" alt="" class="whatsapp"/>  Christo John</a>
     
-            <a class="btn btn-danger" href="https://api.whatsapp.com/send?phone=919188785036&text=Hello%2C%0AI%20want%20to%20join%20muLearn%20whatsapp%20group.%20"> <img src="${imgWhatsapp}" alt="" class="whatsapp"> Binnet Binoy</a>
+            <a class="btn btn-danger" href="https://api.whatsapp.com/send?phone=919447356497&text=Hello%2C%20My%20name%20is%20%5BYour%20name%5D%20and%20I%20am%20%5Byour%20relationship%20with%20MAC%20Ramapuram%5D.%20I%20would%20like%20to%20join%20the%20muLearn%20whatsapp%20group%20of%20MAC%20Ramapuram.%20Please%20add%20me%20to%20the%20group.%20Thank%20you."> <img src="${imgWhatsapp}" alt="" class="whatsapp"/> Sir. Sunil K Joseph</a>
         `;
   }
 
