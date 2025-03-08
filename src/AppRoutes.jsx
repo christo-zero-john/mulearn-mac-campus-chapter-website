d4772c02f6578bb4c67da339284aa59f682ceab2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-03-08 14:03:30 +0530

    Found modified or deleted file,  src/AppRoutes.jsx. Committing Via Global Bash Script:git-status-automated.bat

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -8,6 +8,8 @@ import "./styles/defaults.css";
 import JoinUs from "./components/joinus/joinus";
 import GetStarted from "./components/get-started/get-started";
 import Execom from "./components/execom/execom";
+import Manifesto from "./components/manifesto/manifesto";
+import Onboarding from "./components/onboarding/onboarding";
 
 function AppRoutes() {
   return (
@@ -20,6 +22,8 @@ function AppRoutes() {
         <Route path="/community/tasks" element={<DemoPath />} />
         <Route path="/community/interest-groups" element={<DemoPath />} />
         <Route path="/campus-chapter/execom" element={<Execom />} />
+        <Route path="/manifesto" element={<Manifesto />} />
+        <Route path="/campus-chapter/onboarding" element={<Onboarding />} />
         <Route path="/community/faq" element={<DemoPath />} />
         <Route path="/campus-chapter/events" element={<DemoPath />} />
       </Routes>
